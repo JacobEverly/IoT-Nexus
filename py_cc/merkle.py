3df78d8d3cd10b26e3672cb23a89f551d27ef3bb
Hau Chu <hchoward1998@gmail.com>
2023-03-17 19:12:53 -0400

    modify merkle tree

--- py_cc/merkle.py
+++ py_cc/merkle.py
@@ -1,34 +1,35 @@
-from .hashes.sha256 import SHA
+from hashlib import sha256
 from math import log2, ceil
 
-def verify_merkle_proof(proof, commitment):
+def verify_merkle_proof(proof, commitment, hash_fn=sha256):
     proof = proof.split(',')
     
     left = proof.pop(0).split(':')[1]
     right = proof.pop(0).split(':')[1]
-    curr_hash = SHA(left + right)
+    curr_hash = hash_fn(left + right).hexdigest()
     while proof:
         pos, hash = proof.pop(0).split(':')
         if pos == 'l':
-            curr_hash = SHA(hash + curr_hash)
+            curr_hash = hash_fn(hash + curr_hash).hexdigest()
         else:
-            curr_hash = SHA(curr_hash + hash)
+            curr_hash = hash_fn(curr_hash + hash).hexdigest()
     
     return curr_hash == commitment
 
 class MerkleTree:
-    def __init__(self, leaves):
+    def __init__(self, leaves, hash_fn=sha256):
         self.leaves = leaves
         self._levels = 1 + ceil(log2(len(leaves)))
         self._treenodes = [None] * self._levels
         self._root = None
+        self.hash_fn = hash_fn
         self._build()
     
     def _build(self):
         if not self.leaves:
-            return SHA("")
+            return self.hash_fn("").hexdigest()
         elif len(self.leaves) == 1:
-            return SHA(str(self.leaves[0]))
+            return self.hash_fn(str(self.leaves[0])).hexdigest()
         else:
             for i in range(self._levels):
                 self._treenodes[i] = [None] * (1 << i)
@@ -36,16 +37,16 @@ class MerkleTree:
             level = self._levels - 1
             for i in range(1 << level):
                 if i < len(self.leaves):
-                    self._treenodes[level][i] = SHA(str(self.leaves[i]))
+                    self._treenodes[level][i] = self.hash_fn(str(self.leaves[i])).hexdigest()
                 else:
-                    self._treenodes[level][i] = SHA("")
+                    self._treenodes[level][i] = self.hash_fn("").hexdigest()
             
             while level > 0:
                 level -= 1
                 for i in range(1 << level):
                     left = self._treenodes[level+1][2*i]
                     right = self._treenodes[level+1][2*i+1]
-                    self._treenodes[level][i] = SHA(left + right)
+                    self._treenodes[level][i] = self.hash_fn(left + right).hexdigest()
             
             self._root = self._treenodes[0][0]
     
