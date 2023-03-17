33365216170f4f0c9562aef38af0f8b414e31ba3
Hau Chu <hchoward1998@gmail.com>
2023-03-17 19:14:39 -0400

    Create Compact Certificate class (not finish yet)

--- main.py
+++ main.py
@@ -1,122 +1,106 @@
 # import random
 from typing import Callable
-from random import randint, randbytes
+from random import SystemRandom
 import random
 import hashlib
-from collections import defaultdict
-import numpy as np
 
 from py_cc.hashes import Poseidon, prime_254, matrix_254_3, round_constants_254_3
-from py_cc.elliptic_curves.baby_jubjub import GeneratePoint
+from py_cc.elliptic_curves.baby_jubjub import curve
+from py_cc.keys import PrivateKey, PublicKey
+from py_cc.ecdsa import Ecdsa
+from py_cc.merkle import MerkleTree, verify_merkle_proof
 
 
 import argparse
 
+class Attestor:
+    def __init__(self, private_key:PrivateKey=None, public_key:PublicKey=None, weight=None):
+        self.private_key = private_key
+        self.public_key = public_key
+        self.weight = weight
 
-class PoseidonHash(Poseidon):
-    def __init__(
-        self,
-        prime=prime_254,
-        security_level=128,
-        input_rate=3,
-        t=3,
-        alpha=5,
-        mds_matrix=matrix_254_3,
-    ):
-        """
-        # TODO - add parameters description
-        """
-        super().__init__(
-            prime, security_level, alpha, input_rate, t, mds_matrix=mds_matrix
-        )
+    def setPrivateKey(self, private_key):
+        self.private_key = private_key
+    
+    def setPublicKey(self, public_key):
+        self.public_key = public_key
 
-    def __str__(self) -> str:
-        return "PoseidonHash"
-
-
-class KeyPairGen:
-    def __init__(self):
-        self.ecc = GeneratePoint
-
-    def getKeyPair(self):
-        secret_key = randint(0, prime_254 - 1)
-        public_key = secret_key * self.ecc
-        return secret_key, public_key
-
-    def getKeyPairs(self, num):
-        """
-        return a dictionary of key pairs in int format (not sure should be in hex or not)
-        key: secret key
-        value: public key (x, y)
-        """
-        print("Generate key pair")
-        pairs = {}
-        for i in range(num):
-            while True:
-                sk, pk = self.getKeyPair()
-                if sk not in pairs:
-                    pairs[sk] = pk
-                    break
-            print(f"Node: {i}")
-            print(f"SK: {sk}")
-            print(f"PK: {pk}")
-            print("------------------")
-        return pairs
+    def setWeight(self, weight):
+        self.weight = weight
 
     def __repr__(self):
-        return f"Hash: {self.hash}, ECC: {self.ecc}"
+        return f"PK: {self.public_key}, SK: {self.private_key}, Weight: {self.weight}"
+
+class CompactCertificate:
+    def __init__(self, hash, curve, attesters=0):
+        self.hash = hash
+        self.curve = curve
+        self.attesters = [Attestor()] * attesters
+        self.attester_tree = None
+        self.signatures = []
+        self.sign_tree = None
+        self.total_weight = 0
+
+    def setAttestors(self):
+        total_weight = 0
+        for i in range(len(self.attesters)):
+            sk = PrivateKey(self.curve)
+            pk = sk.get_public_key()
+            weight = SystemRandom().randrange(1, 1000)
+            total_weight += weight
+
+            self.attesters[i].setPrivateKey(sk)
+            self.attesters[i].setPublicKey(pk)
+            self.attesters[i].setWeight(weight)
+            
+        self.attesters = sorted(self.attesters, key=lambda x: x.weight, reverse=True)
+        self.total_weight = total_weight
+
+    def signMessage(self, message):
+        for i in range(len(self.attesters)):
+            sk = self.attesters[i].private_key
+            signature = Ecdsa.sign(message, sk, hash_fn=self.hash.run)
+            self.signatures.append(signature)
+
+    def verifySignatures(self, message):
+        for i in range(len(self.attesters)):
+            pk = self.attesters[i].public_key
+            signature = self.signatures[i]
+            if not Ecdsa.verify(message, signature, pk, hash_fn=self.hash.run):
+                return False
+        return True
+    
+    def buildAttesterTree(self):
+        pks = [attester.public_key.toString() for attester in self.attesters]
+        self.attester_tree = MerkleTree(pks, hash_fn=self.hash.run)
+
+    # Create merkle tree including Witness (Signed message by SK) and attestors range which is their spot in the provenweight
+    def buildSignTree(self):
+        weights = [attester.weight for attester in self.attesters]
+        signatures = [signature.toString()+","+weight for signature, weight in zip(self.signatures, weights)]
+        self.sign_tree = MerkleTree(signatures, hash_fn=self.hash.run)
 
 
-def create_weights(PKs):
-    weights = ()
-    attesters = ()
 
-    for i in range(0, len(PKs)):
-        weight = np.random.randint(1, 1000)
-        weights.append(weight)
+def verifyMerkleTree(tree:MerkleTree, commitment, proof, hash_fn:Callable[[str], int]):
+    index = random_oracle(tree.get_root())
+    proof = tree.get_proof(index)
 
-    for i in range(len(PKs)):
-        attesters = (PKs[i], weights[i])
+    return verify_merkle_proof(proof, commitment, hash_fn=hash_fn)
 
-    attesters = sorted(attesters, key=lambda x: x[1], reverse=True)
-    provenWeight = sum(weights)
-
-    return attesters, provenWeight
-
-
-# Create Merkle tree of attestors (PK and weights)
-def build_merkle_tree(elements):
-    if len(elements) == 1:
-        return elements[0][0].encode()
-
-    # Combine adjacent pairs of elements and hash the result
-    pairs = [
-        (elements[i][0].encode() + elements[i + 1][0].encode())
-        for i in range(0, len(elements), 2)
-    ]
-    if len(elements) % 2 == 1:
-        pairs.append(elements[-1][0].encode() * 2)
-
-    # Hash each pair of elements together and store the resulting hash value
-    hashes = [hashlib.sha256(pair).digest() for pair in pairs]
-
-    # Recursively build the Merkle tree from the list of hash values
-    return build_merkle_tree(hashes)
 
 
 # create range for each attestor by adding their weight to the previous attestor's range and marking the endpoints
-def create_range(attesters, provenWeight):
-    attestor_range = ()
-    range_index = 0
+def create_weight_range(attesters):
+    attesters_range = []
+    left_value = 0
 
-    for i in range(len(attesters)):
-        attestor_range = (attesters[i], range_index, range_index + attesters[i][1])
-        range_index += attesters[i][1]
+    for attester in attesters:
+        attesters_range.append(f"w:(${attester.weight},${left_value},${left_value + attester.weight})")
+        left_value += attester.weight
 
-    return attestor_range
+    return attesters_range
 
-
-# Create merkle tree including Witness (Signed message by SK) and attestors range which is their spot in the provenweight
 
 
 # Create Random Oracle that tells us the subrange that the prover should select a tree from
@@ -136,18 +120,19 @@ if __name__ == "__main__":
         "-n", "--num", type=int, default=1, help="Number of key pairs to generate"
     )
     args = parser.parse_args()
-    keyGen = KeyPairGen()
-    pairs = keyGen.getKeyPairs(args.num)
 
-    # attesters, provenWeight = create_weights(pairs)
-    # # Extract the attesters and weights into separate lists
-    # attesters = [attester[0] for attester in attesters]
-    # weights = [attester[1] for attester in attesters]
-
-    # # Combine each attester with its weight into a tuple
-    # elements = list(zip(attesters, weights))
-
-    # # Build the Merkle tree from the list of attesters and weights
-    # root_hash = build_merkle_tree(elements)
-    # # Print the root hash of the Merkle tree
-    # print("Merkle root hash:", root_hash.hex())
+    hash = Poseidon(
+            prime_254,
+            128,
+            5,
+            3,
+            3,
+            full_round=8,
+            partial_round=57,
+            mds_matrix=matrix_254_3,
+            rc_list=round_constants_254_3,
+        )
+    CC = CompactCertificate(hash, curve, args.num)
+    CC.setAttestors()
+    print("Attestors: ", CC.attesters)
+    
