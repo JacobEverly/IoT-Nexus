0ce43ea9c2981c32affe7ed59c894b9645a9f7c6
Hau Chu <hchoward1998@gmail.com>
2023-03-21 22:40:03 -0400

    complete eddsa and test

--- py_cc/eddsa.py
+++ py_cc/eddsa.py
@@ -5,27 +5,41 @@ from .hashes.sha256 import SHA
 from .signature import Signature
 from binascii import hexlify, unhexlify
 
-class Signature:
-    def __init__(self, r, s, recovery_id=None):
-        self.r = r
-        self.s = s
-        self.recovery_id = recovery_id
-
-    def toString(self):
-        return f"r:{self.r},s:{self.s}"
-
-    def __str__(self):
-        return f"r: {self.r}, s: {self.s}"
-     
-    def __repr__(self):
-        return f"Signature({self.r}, {self.s}, {self.recovery_id})"
+# Reference: https://medium.com/asecuritysite-when-bob-met-alice/whats-the-difference-between-ecdsa-and-eddsa-e3a16ee0c966#:~:text=For%20improved%20security%2C%20ECDSA%20supports,compatibility%20with%20Bitcoin%20and%20Ethereum.
 
 class Eddsa:
 
     @classmethod
     def sign(cls, message, private_key:PrivateKey, public_key:PublicKey, hash_fn=SHA):
-        pass
+        curve = private_key.curve
+        generator = BabyJubjubPoint(curve.G[0], curve.G[1])
+        en_msg = message.encode("utf-8")
+
+        sk_hash = hash_fn(private_key.secret).digest()
+        hash = hash_fn(sk_hash + en_msg).digest()
+
+        r = int.from_bytes(hash, "big") % curve.n
+        R = (r * generator)
+        _R = R.y.to_bytes(curve.length, "big")
+
+        h = int.from_bytes(hash_fn(_R + public_key.toBytes() + en_msg).digest(), "big") % curve.n
+
+        # s = int.from_bytes(sk_hash, "big")
+        S = (r + h * private_key.secret) % curve.n
+        return Signature(R, S)
     
     @classmethod
     def verify(cls, message, signature:Signature, public_key: PublicKey, hash_fn=SHA):
-        pass
+        curve = public_key.curve
+        generator = BabyJubjubPoint(curve.G[0], curve.G[1])
+        en_msg = message.encode("utf-8")
+
+        R = signature.r
+        S = signature.s
+        _R = R.y.to_bytes(curve.length, "big")
+        k = int.from_bytes(hash_fn(_R + public_key.toBytes() + en_msg).digest(), "big") % curve.n
+
+        P1 = S * generator
+        P2 = R + k * public_key.point
+        # print(P1, P2)
+        return P1 == P2
