b407c60b60c4db32685a7aad95b34bc331c05da9
Samhitha Tarra <samhitht@uci.edu>
2023-04-13 12:58:03 -0400

    adding routes flask setup

--- routes.py
+++ routes.py
@@ -1,11 +1,13 @@
 from flask import Flask, render_template, request, redirect
 import sqlite3
 from flask import jsonify
+from flask import make_response
 import json
+import subprocess
 from flask_cors import CORS
 
-# running instructions:
-# start the server with: FLASK_APP=routes.py flask run
+#
+
 app = Flask(__name__)
 CORS(app)
 
@@ -16,8 +18,16 @@ def upload():
 
     # you now have access to the csv files uploaded and can do whatever you want with them
     print(file1.filename, file2.filename)
+
+
+    subprocess.call(['python3', 'main.py'])
+    
     
     return jsonify({'status': 'success'})
+
+
+
+
 
 
 
