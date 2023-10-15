7e9c6d56486ff7b54fa36a6133c51451e3399b30
Hau Chu <hchoward1998@gmail.com>
2023-10-15 14:37:13 -0400

    add test case

--- CC_contract/test/CCSender.test.ts
+++ CC_contract/test/CCSender.test.ts
@@ -34,6 +34,13 @@ describe("CompactCertificateSender", function () {
       expect(data[0]).to.equal("Hello");
     });
 
+    it("Store multiple data", async function () {
+      await compactCertificateSender.storeData("World");
+      const data = await compactCertificateSender.getData(owner.address);
+      expect(data[0]).to.equal("Hello");
+      expect(data[1]).to.equal("World");
+    });
+
     it("Delete the data", async function () {
       await compactCertificateSender.storeData("Hello");
       await compactCertificateSender.clearData(owner.address);
@@ -41,18 +48,45 @@ describe("CompactCertificateSender", function () {
       expect(data.length).to.equal(0);
     });
 
-    it("summarize", async function () {
-      await compactCertificateSender.storeData("Hello");
-      await compactCertificateSender.summarizeData();
-      const messageHash = await compactCertificateSender.getSummarizedMessage(
-        owner.address
-      );
-      const abiEncoded = utils.defaultAbiCoder.encode(
-        ["string[]"],
-        [["Hello"]]
-      );
-      const abiEncodedHash = utils.keccak256(abiEncoded);
-      expect(messageHash).to.equal(abiEncodedHash);
+    describe("Signature Testing", async function () {
+      it("summarize", async function () {
+        await compactCertificateSender.storeData("Hello");
+        await compactCertificateSender.summarizeData();
+        const messageHash = await compactCertificateSender.getSummarizedMessage(
+          owner.address
+        );
+        const abiEncoded = utils.defaultAbiCoder.encode(
+          ["string[]"],
+          [["Hello"]]
+        );
+        const abiEncodedHash = utils.keccak256(abiEncoded);
+        expect(messageHash).to.equal(abiEncodedHash);
+      });
+
+      it("summarize multiple data", async function () {
+        await compactCertificateSender.storeData("World");
+        await compactCertificateSender.summarizeData();
+        const messageHash = await compactCertificateSender.getSummarizedMessage(
+          owner.address
+        );
+        const abiEncoded = utils.defaultAbiCoder.encode(
+          ["string[]"],
+          [["Hello", "World"]]
+        );
+        const abiEncodedHash = utils.keccak256(abiEncoded);
+        expect(messageHash).to.equal(abiEncodedHash);
+      });
+
+      it("Delete the data", async function () {
+        await compactCertificateSender.clearDataSummary(owner.address);
+        const data = await compactCertificateSender.getSummarizedMessage(
+          owner.address
+        );
+
+        expect(data).to.equal(
+          "0x0000000000000000000000000000000000000000000000000000000000000000"
+        );
+      });
     });
   });
 });
