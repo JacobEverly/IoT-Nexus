0520a4db0b2c9e9c9982a083bfcf9a209aad3ba1
Maureen Hsu <maureen.hsu@gmail.com>
2023-11-06 01:14:36 -0500

    adjust user data page

--- frontend/src/pages/dashboard/profile.jsx
+++ frontend/src/pages/dashboard/profile.jsx
@@ -11,6 +11,7 @@ import {
   Switch,
   Tooltip,
   Button,
+  Progress,
 } from "@material-tailwind/react";
 import {
   HomeIcon,
@@ -18,10 +19,56 @@ import {
   Cog6ToothIcon,
   PencilIcon,
 } from "@heroicons/react/24/solid";
-import { Link } from "react-router-dom";
-import { ProfileInfoCard, MessageCard } from "@/widgets/cards";
-import { platformSettingsData, conversationsData, projectsData } from "@/data";
-
+import Blockies from 'react-blockies';
+import { getMessages } from "@/restApis/getMessages";
+import "@/../public/css/tailwind.css";
+let messages = await getMessages();
+let validator_address = null;
+while (validator_address == null) {
+  for (let i = 0; i < messages.length; i++) {
+    if (messages[i].signed_validators.length > 0) {
+      validator_address = messages[i].signed_validators[0].wallet_address;
+      break;
+    }
+  }
+}
+let total_weight = 0;
+let num_v = 0;
+messages.map(({ signed_validators }, key) => {
+  signed_validators.map(({ weight }, key) => {
+    total_weight += weight;
+  })
+  num_v += 1;
+  total_weight = total_weight / num_v;
+})
+function SignComponent({ signed_validators, validator_address }) {
+  let signed = false;
+  signed_validators.map(({ wallet_address }, key) => {
+    if (wallet_address == validator_address) {
+      signed = true;
+    }
+  })
+  if (!signed) {
+    return (
+      <div className="container"><Button color="green" size="">Approve</Button> </div>
+    )
+  } else {
+    return (
+      <div><Button disabled color="light-green">Signed</Button></div>
+    )
+  }
+}
+function getCompletionRate(signed_validators) {
+  let signed_weight = 0;
+  signed_validators.map(({ weight }, key) => {
+    signed_weight += weight;
+  })
+  let rate = signed_weight / total_weight;
+  while (rate > 1) {
+    rate /= 10;
+  }
+  return rate.toPrecision(2);
+}
 export function UserData() {
   return (
     <>
@@ -32,26 +79,20 @@ export function UserData() {
         <CardBody className="p-4">
           <div className="mb-10 flex items-center justify-between gap-6">
             <div className="flex items-center gap-6">
-              <Avatar
-                src="/img/bruce-mars.jpeg"
-                alt="bruce-mars"
-                size="xl"
-                className="rounded-lg shadow-lg shadow-blue-gray-500/40"
+              <Blockies
+                data-testid="avatar"
+                seed={validator_address.toLowerCase() || ""}
+                scale={7}
+                size={7}
               />
               <div>
-                <Typography variant="h5" color="blue-gray" className="mb-1">
-                  Richard Davis
-                </Typography>
-                <Typography
-                  variant="small"
-                  className="font-normal text-blue-gray-600"
-                >
-                  CEO / Co-Founder
+                <Typography variant="h6" color="blue-gray" className="mb-1">
+                  {validator_address}
                 </Typography>
               </div>
             </div>
-            <div className="w-96">
-              <Tabs value="app">
+            {/* <div className="w-96"> */}
+            {/* <Tabs value="app">
                 <TabsHeader>
                   <Tab value="app">
                     <HomeIcon className="-mt-1 mr-2 inline-block h-5 w-5" />
@@ -66,38 +107,17 @@ export function UserData() {
                     Settings
                   </Tab>
                 </TabsHeader>
-              </Tabs>
-            </div>
+              </Tabs> */}
+            {/* </div> */}
           </div>
           <div className="gird-cols-1 mb-12 grid gap-12 px-4 lg:grid-cols-2 xl:grid-cols-3">
             <div>
               <Typography variant="h6" color="blue-gray" className="mb-3">
-                Platform Settings
+                Generate Your Message
               </Typography>
-              <div className="flex flex-col gap-12">
-                {platformSettingsData.map(({ title, options }) => (
-                  <div key={title}>
-                    <Typography className="mb-4 block text-xs font-semibold uppercase text-blue-gray-500">
-                      {title}
-                    </Typography>
-                    <div className="flex flex-col gap-6">
-                      {options.map(({ checked, label }) => (
-                        <Switch
-                          key={label}
-                          id={label}
-                          label={label}
-                          defaultChecked={checked}
-                          labelProps={{
-                            className: "text-sm font-normal text-blue-gray-500",
-                          }}
-                        />
-                      ))}
-                    </div>
-                  </div>
-                ))}
-              </div>
+
             </div>
-            <ProfileInfoCard
+            {/* <ProfileInfoCard
               title="Profile Information"
               description="Hi, I'm Alec Thompson, Decisions: If you can't decide, the answer is no. If two equally difficult paths, choose the one more painful in the short term (pain avoidance is creating an illusion of equality)."
               details={{
@@ -118,8 +138,8 @@ export function UserData() {
                   <PencilIcon className="h-4 w-4 cursor-pointer text-blue-gray-500" />
                 </Tooltip>
               }
-            />
-            <div>
+            /> */}
+            {/* <div>
               <Typography variant="h6" color="blue-gray" className="mb-3">
                 Platform Settings
               </Typography>
@@ -136,79 +156,132 @@ export function UserData() {
                   />
                 ))}
               </ul>
-            </div>
+            </div> */}
           </div>
           <div className="px-4 pb-4">
             <Typography variant="h6" color="blue-gray" className="mb-2">
-              Projects
+              Your Data
             </Typography>
-            <Typography
-              variant="small"
-              className="font-normal text-blue-gray-500"
-            >
-              Architects design houses
-            </Typography>
-            <div className="mt-6 grid grid-cols-1 gap-12 md:grid-cols-2 xl:grid-cols-4">
-              {projectsData.map(
-                ({ img, title, description, tag, route, members }) => (
-                  <Card key={title} color="transparent" shadow={false}>
-                    <CardHeader
-                      floated={false}
-                      color="gray"
-                      className="mx-0 mt-0 mb-4 h-64 xl:h-40"
-                    >
-                      <img
-                        src={img}
-                        alt={title}
-                        className="h-full w-full object-cover"
-                      />
-                    </CardHeader>
-                    <CardBody className="py-0 px-1">
-                      <Typography
-                        variant="small"
-                        className="font-normal text-blue-gray-500"
-                      >
-                        {tag}
-                      </Typography>
-                      <Typography
-                        variant="h5"
-                        color="blue-gray"
-                        className="mt-1 mb-2"
-                      >
-                        {title}
-                      </Typography>
-                      <Typography
-                        variant="small"
-                        className="font-normal text-blue-gray-500"
-                      >
-                        {description}
-                      </Typography>
-                    </CardBody>
-                    <CardFooter className="mt-6 flex items-center justify-between py-0 px-1">
-                      <Link to={route}>
-                        <Button variant="outlined" size="sm">
-                          view project
-                        </Button>
-                      </Link>
-                      <div>
-                        {members.map(({ img, name }, key) => (
-                          <Tooltip key={name} content={name}>
-                            <Avatar
-                              src={img}
-                              alt={name}
-                              size="xs"
-                              variant="circular"
-                              className={`cursor-pointer border-2 border-white ${key === 0 ? "" : "-ml-2.5"
+            <CardBody className="overflow-x-scroll px-0 pt-0 pb-2">
+              <table className="w-full min-w-[640px] table-auto">
+                <thead>
+                  <tr>
+                    {["messages", "members", "company", "completion", "create at", "decision"].map(
+                      (el) => (
+                        <th
+                          key={el}
+                          className="border-b border-blue-gray-50 py-3 px-6 text-left"
+                        >
+                          <Typography
+                            variant="small"
+                            className="text-[11px] font-medium uppercase text-blue-gray-400"
+                          >
+                            {el}
+                          </Typography>
+                        </th>
+                      )
+                    )}
+                  </tr>
+                </thead>
+                <tbody>
+                  {messages.map(
+                    ({ message, signed_validators, created_by, created_at }, key) => {
+                      const className = `py-3 px-5 ${key === messages.length - 1
+                        ? ""
+                        : "border-b border-blue-gray-50"
+                        }`;
+
+                      return (
+                        <tr key={message}>
+                          <td className={className}>
+                            <div className="flex items-center gap-4">
+
+                              {/* <Avatar src={img} alt={message} size="sm" /> */}
+                              <Typography
+                                variant="small"
+                                color="blue-gray"
+                                className="font-bold"
+                              >
+                                {message}
+                              </Typography>
+                            </div>
+                          </td>
+                          <td className={`${className}`}>
+                            <div className="members-container">
+                              {signed_validators.map(({ wallet_address, weight }, key) => (
+                                <Tooltip key={wallet_address} content={wallet_address}>
+                                  {/* <Avatar
+                                src={img}
+                                alt={name}
+                                size="xs"
+                                variant="circular"
+                                className={`cursor-pointer border-2 border-white ${
+                                  key === 0 ? "" : "-ml-2.5"
                                 }`}
-                            />
-                          </Tooltip>
-                        ))}
-                      </div>
-                    </CardFooter>
-                  </Card>
-                )
-              )}
-            </div>
+                              /> */}
+                                  <Blockies
+                                    data-testid="avatar"
+                                    seed={wallet_address.toLowerCase() || ""}
+                                    scale={5}
+                                    size={3}
+                                    className="rounded-full"
+                                  />
+                                </Tooltip>
+                              ))}
+                            </div>
+                          </td>
+                          <td className={`${className}`}>
+                            <div className="container">
+                              <Blockies
+                                data-testid="avatar"
+                                seed={created_by.toLowerCase() || ""}
+                                scale={5}
+                                size={5}
+                                className="rounded-full"
+                              />
+                              <Typography
+                                variant="small"
+                                className="text-xs font-medium text-blue-gray-600"
+                                style={{ paddingLeft: "10px" }}
+                              >
+                                {created_by}
+                              </Typography>
+                            </div>
+                          </td>
+                          <td className={className}>
+                            <div className="w-10/12">
+                              <Typography
+                                variant="small"
+                                className="mb-1 block text-xs font-medium text-blue-gray-600"
+                              >
+                                {getCompletionRate(signed_validators) * 100}%
+                              </Typography>
+                              <Progress
+                                value={100}
+                                variant="gradient"
+                                color={99 === 100 ? "green" : "blue"}
+                                className="h-1"
+                              />
+                            </div>
+                          </td>
+                          <td className={className}>
+                            <Typography
+                              variant="small"
+                              className="text-xs font-medium text-blue-gray-600"
+                            >
+                              {created_at}
+                            </Typography>
+                          </td>
+                          <td className={`flex-center-wrap ${className}`}>
+                            <SignComponent signed_validators={signed_validators} validator_address={validator_address} />
+                          </td>
+                        </tr>
+                      );
+                    }
+                  )}
+                </tbody>
+              </table>
+            </CardBody>
           </div>
         </CardBody>
       </Card>
